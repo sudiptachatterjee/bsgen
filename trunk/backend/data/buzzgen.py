cdcedf16fb58d1514f8dbe750fc452518bc54e71
Sudipta Chatterjee <sudipta.referral@gmail.com>
2013-10-10 07:00:31 +0000

    Adding frankenspeak words

---
+++ trunk/backend/data/buzzgen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from random import choice
+
+adj = open('adjectives.txt','r').readlines()
+adv = open('adverbs.txt','r').readlines()
+nouns = open('nouns.txt','r').readlines()
+verbs = open('verbs.txt','r').readlines()
+
+def sc(wordlist):
+    return strip(choice(wordlist))
+
+print ' '.join([choice(adv), choice(verbs), choice(adj), choice(nouns)])
+
+
